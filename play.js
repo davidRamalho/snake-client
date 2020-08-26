778ee22999ec442e0869e14fdcef5588f979fb4e
David Ramalho <david.m.ramalho@gmail.com>
2020-08-26 21:59:47 +0000

    added exit command

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 const { connect } = require('./client');
+const { generateKeyPair } = require('crypto');
 
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleInputUser = (key) => {   
+    if (key === '\u0003') {
+      process.exit();
+    }
+    
+  })
+  
+  return stdin;
+}
+setupInput();
+
