9d45d40d82c355cb82905f8cfba1f410fe5dc04d
David Ramalho <david.m.ramalho@gmail.com>
2020-08-26 22:51:23 +0000

    adding movement commands

--- input.js
+++ input.js
@@ -1,11 +1,24 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
+    
   stdin.on('data', handleInputUser = (key) => {   
     if (key === '\u0003') {
       process.exit();
+    }
+    if (key === 'w'){
+      conn.write('Move: up');
+    } else if (key === 's') {
+      conn.write('Move: down');
+    } else if (key === 'a') {
+      conn.write('Move: left');
+    } else if (key === 'd') {
+      conn.write('Move: right');
     }
     
   })
--- play.js
+++ play.js
@@ -3,7 +3,5 @@ const { generateKeyPair } = require('crypto');
 const { setupInput } = require('./input')
 
 console.log('Connecting ...');
-connect();
 
-setupInput();
-
+setupInput(connect());
